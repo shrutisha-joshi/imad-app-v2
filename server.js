46b2011b3128eb0318b22c109d049763f17b48e0
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-02-14 19:55:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one.html',function (req, res){
+app.get('/article-one', function (req, res){
     res.send("this is my first article and will get back soon");
 });
 app.get('/ui/style.css', function (req, res) {
