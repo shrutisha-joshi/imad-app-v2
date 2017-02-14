e6afef84e8bde92d3668c96151e2254c469053c8
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-02-14 19:56:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res){
     res.send("this is my first article and will get back soon");
 });
+app.get('/article-two', function (req, res){
+    res.send("this is my first article and will get back soon");
+});
+app.get('/article-three', function (req, res){
+    res.send("this is my first article and will get back soon");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
