0b8e7cf519ee935e36afd92ab3e3af42282724b4
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-02-27 17:48:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,12 @@
-console.log('Loaded!');
-
-//change the text of the html file
-var element=document.getElementById("main-text");
-element.innerHTML="new value i changed";
-// move the image
-var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+"px";
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-    img.style.marginLeft="100px";
-}
+var button = document.getElementById("counter");
+var counter =0;
+button.onclick = function(){
+    //make request
+    
+    //capture the status
+    
+    //render the variable in the correct span
+    var counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+};
