259b5af71113f3bb69f311d42d5cdcb61364ddcf
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-02-27 16:08:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element=document.getElementById("main-text");
 element.innerHTML="new value i changed";
 // move the image
 var img=document.getElementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+"px";
+}
 img.onclick=function(){
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft="100px";
 }
