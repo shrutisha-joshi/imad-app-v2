09b3ab403e6a0a3eee531bc08fa39947cacb4ac5
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-02-27 16:02:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //change the text of the html file
 var element=document.getElementById("main-text");
 element.innerHTML="new value i changed";
+// move the image
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft="100px";
+}
