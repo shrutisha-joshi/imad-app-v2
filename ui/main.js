f5484a1f6ee4dd12ff85f419b485b87268a62373
shrutisha-joshi <shrutisha10sep98@gmail.com>
2017-03-01 21:37:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,20 @@ button.onclick = function(){
     request.open('GET','http://shrutisha-joshi.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick()=function(){
+   //make the request to the server
+   //capture the list of names and render it as a list
+   var names=['name1','name2','name3','name4'];
+   var list='';
+   for(var i=0;i<names.length;i++)
+   {
+       list+='<li>'+names[i]+'</li>';
+       
+   }
+   var ul=document.getElementById("namelist");
+   ul.innerHTML=list;
+};
